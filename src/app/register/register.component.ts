d11dd6a47c5c3467ccc2173aa0e8a6d7ecd27ed4
Olivier Ville <olivier.ville@free.fr>
2020-09-15 12:15:38 +0200

    Implement Register Component

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,15 +1,56 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
+
+import { AuthorizationService } from './../service/authorization.service'
+
+
+
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+	
+	confirmCode: boolean = false;
+	codeWasConfirmed: boolean = false;
+	error:string = "";
+		
+	constructor(private auth: AuthorizationService,	private _router: Router) {	}
+	
+	register(form: NgForm) {
+		const email = form.value.email;
+		const password = form.value.password;
+		
+		this.auth.register(email, password).suscribe(
+			(data) => {
+				this.confirmCode = true;
+			}, 
+			(err) => {
+				this.error = "Registration Error has Occurred";
+			}
+		);
+	}
+	
+	validateAuthCode(form: NgForm) {
+		const code = form.value.code;
+		
+		this.auth.confirmAuthCode(code).suscribe(
+			(data) => {
+				this.codeWasConfirmed = true;
+				this.confirmCode = false;
+			}, 
+			(err) => {
+				console.log(err);
+				this.error = "Confirm Authorization Error has Occurred";
+			}
+		);
+	}
+	
+	ngOnInit(): void {
+		
+	}
 
 }
