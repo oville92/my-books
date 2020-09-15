0f0389c69cfa6b94656f31bc7fb588341c881427
Olivier Ville <olivier.ville@free.fr>
2020-09-15 10:09:26 +0200

    fix app routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import [HomeComponent} from "./home/home.component" ;
-import [RestapiComponent} from "./restapi/restapi.component" ;
-import [LoginComponent} from "./login/login.component" ;
-import [RegisterComponent} from "./register/register.component" ;
+import {HomeComponent} from "./home/home.component";
+import {RestapiComponent} from "./restapi/restapi.component";
+import {LoginComponent} from "./login/login.component";
+import {RegisterComponent} from "./register/register.component";
 
 const routes: Routes = [
 	{path : '', component: HomeComponent},
